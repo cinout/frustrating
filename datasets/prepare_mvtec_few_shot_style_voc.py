f82d30b26c6650d85c91ce7e9593d90742f69e64
Jane Doe <haitianh@2073L-207327-M.local>
2023-02-16 16:10:08 +1100

    update

--- datasets/prepare_mvtec_few_shot_style_voc.py
+++ datasets/prepare_mvtec_few_shot_style_voc.py
@@ -3,9 +3,71 @@ import copy
 import os
 import random
 import xml.etree.ElementTree as ET
-
 import numpy as np
-from fsdet.utils.file_io import PathManager
+
+# from fsdet.utils.file_io import PathManager
+
+from iopath.common.file_io import (
+    HTTPURLHandler,
+    OneDrivePathHandler,
+    PathHandler,
+    PathManager as PathManagerBase,
+)
+
+__all__ = ["PathManager", "PathHandler"]
+
+
+PathManager = PathManagerBase()
+"""
+This is a detectron2 project-specific PathManager.
+We try to stay away from global PathManager in fvcore as it
+introduces potential conflicts among other libraries.
+"""
+
+
+class Detectron2Handler(PathHandler):
+    """
+    Resolve anything that's hosted under detectron2's namespace.
+    """
+
+    PREFIX = "detectron2://"
+    S3_DETECTRON2_PREFIX = "https://dl.fbaipublicfiles.com/detectron2/"
+
+    def _get_supported_prefixes(self):
+        return [self.PREFIX]
+
+    def _get_local_path(self, path):
+        name = path[len(self.PREFIX) :]
+        return PathManager.get_local_path(self.S3_DETECTRON2_PREFIX + name)
+
+    def _open(self, path, mode="r", **kwargs):
+        return PathManager.open(self._get_local_path(path), mode, **kwargs)
+
+
+class FsDetHandler(PathHandler):
+    """
+    Resolve anything that's in FsDet model zoo.
+    """
+
+    PREFIX = "fsdet://"
+    URL_PREFIX = "http://dl.yf.io/fs-det/models/"
+
+    def _get_supported_prefixes(self):
+        return [self.PREFIX]
+
+    def _get_local_path(self, path):
+        name = path[len(self.PREFIX) :]
+        return PathManager.get_local_path(self.URL_PREFIX + name)
+
+    def _open(self, path, mode="r", **kwargs):
+        return PathManager.open(self._get_local_path(path), mode, **kwargs)
+
+
+PathManager.register_handler(HTTPURLHandler())
+PathManager.register_handler(OneDrivePathHandler())
+PathManager.register_handler(Detectron2Handler())
+PathManager.register_handler(FsDetHandler())
+
 
 VOC_CLASSES = [
     "aeroplane",
